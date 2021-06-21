1f69714df7f2ce7291f845808bf96502940c9cd2
geekbrains <6apsyk@mail.ru>
2021-06-21 19:15:00 +0300

    added lesoon8

---
+++ app.js
@@ -0,0 +1,45 @@
+'use strict';
+
+let fitlerPopup = document.querySelector('.filterPopup');
+let fitlerLabel = document.querySelector('.filterLabel');
+let filterIcon = document.querySelector('.filterIcon');
+
+fitlerLabel.addEventListener('click', function() {
+    fitlerPopup.classList.toggle('hidden');
+    fitlerLabel.classList.toggle('filterLabelPink');
+    filterIcon.classList.toggle('filterIconPink');
+
+    if (filterIcon.getAttribute('src') === 'images/filter.svg') {
+        filterIcon.setAttribute('src', 'images/filterHover.svg')
+    } else {
+        filterIcon.setAttribute('src', 'images/filter.svg')
+    }
+});
+
+let filterHeaders = document.querySelectorAll('.filterCategoryHeader');
+filterHeaders.forEach(function(header) {
+    header.addEventListener('click', function(event) {
+        event.target.nextElementSibling.classList.toggle('hidden');
+    })
+});
+
+let filterSizes = document.querySelector('.filterSizes');
+let filterSizeWrap = document.querySelector('.filterSizeWrap');
+filterSizeWrap.addEventListener('click', function() {
+    filterSizes.classList.toggle('hidden');
+});
+
+function addEventListenerForAddToCardButtons(){
+    const addToBtns = document.querySelectorAll('button[data-productId]');
+    addToBtns.forEach( function (button){
+        button.addEventListener('click', addedProductHandler);
+    });
+}
+
+function addedProductHandler(event){
+    const productId = event.currentTarget.getAttribute('data-productId');
+    // console.log(productId);
+    addProductIntoBasket(productId);
+}
+
+addEventListenerForAddToCardButtons();
